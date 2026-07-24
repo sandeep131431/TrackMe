004439a04dcf273a96abe294ebeea07b72a4787e
Sandeep Kumar <skumar131431@gmail.com>
2026-07-24 22:05:30 +0530

    s

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
 
     try {
       const response = await fetch(
-        `https://track-me-gweb.vercel.app/api/${linkId}`
+        `https://track-me-gweb.vercel.app/api/location/${linkId}`
       );
       
       if (response.status === 404) {
